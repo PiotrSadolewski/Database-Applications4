2519e86e6c9286713f7e815553b27426081bb450
PiotrSadolewski <piotr.sadolewski00@gmail.com>
2022-02-16 21:40:16 +0100

    commit

---
+++ LinqTutorials/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task11();
+            foreach (object obj in t) 
+            {
+                Console.WriteLine(obj.ToString());
+            }
+            int[] array = {1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1};
+            var tmp = LinqTasks.Task13(array);
+            Console.WriteLine(tmp.ToString());
+        }
+    }
+}
